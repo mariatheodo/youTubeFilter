b5d3fdb66254be1214efc123fa121dd91ba68814
mariath <maria.theodo@gmail.com>
2017-03-08 14:24:17 +0200

    gives expected results except when first loads

--- scripts/background.js
+++
@@ -1,6 +0,0 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-	console.log("Blocking all!");
-	chrome.tabs.executeScript( {
-		code: 'document.body.style.backgroundColor = "red"'
-	});
-});
--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,9 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
 	var list = document.getElementsByTagName("a");
-	console.log(list);
+	var blocked = 0;
 	for (var i =0; i < list.length; i++) {
 		if (list[i].innerHTML.toLowerCase().includes("winx")) {
-			console.log(list[i]);
+			blocked++;
+		       list[i].parentElement.parentElement.parentElement.style.display = 'none';
+		}
+	}
+});
+
+document.addEventListener('scroll', function() {
+	var list = document.getElementsByTagName("a");
+	var blocked = 0;
+	for (var i =0; i < list.length; i++) {
+		if (list[i].innerHTML.toLowerCase().includes("winx")) {
+			blocked++;
+		       list[i].parentElement.parentElement.parentElement.style.display = 'none';
+		}
+	}
+});
+
+document.addEventListener('wheel', function() {
+	var list = document.getElementsByTagName("a");
+	var blocked = 0;
+	for (var i =0; i < list.length; i++) {
+		if (list[i].innerHTML.toLowerCase().includes("winx")) {
+			blocked++;
 		       list[i].parentElement.parentElement.parentElement.style.display = 'none';
 		}
 	}
